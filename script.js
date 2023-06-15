a17e9cee355304ba38f90b99081d260e3ba2bbe4
Hiyorina1 <131929299+Hiyorina1@users.noreply.github.com>
2023-06-15 10:38:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+// $(".slider").bxSlider({
+//     mode: 'fade',
+//     captions: true,
+//     slideWidth: 600,
+//     auto: true
+// });
+//   });
+
+
+$(document).ready(function(){
+    $(".slider").bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 600
+    });
+  });
